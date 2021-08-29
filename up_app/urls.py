70487000f2ae2c0109319ed851047d6856d4714a
denisCenter2m <47447276+DenisYavetsky@users.noreply.github.com>
2021-08-29 20:47:10 +0300

    lessonX

--- up_app/urls.py
+++ up_app/urls.py
@@ -6,20 +6,18 @@ from django.views.static import serve
 from django.conf.urls import url
 
 urlpatterns = [
-    path('parse/', views.parse, name='parse'),
-    path('filter123/', views.filter123, name='filter123'),
-    path('test/', views.test, name='test'),
-    path('catalog/', views.catalog, name='catalog'),
-    path('plan/', views.plan, name='plan'),
-    path('product_add/', views.product_add, name='product_add'),
-    path('product_change/', views.product_change, name='product_change'),
-    path('get_delivery/<int:pk>', views.get_delivery, name='get_delivery'),
-    path('cart/', views.cart, name='cart'),
-    path('cart/delete/<int:pk>', views.cart_delete, name='cart_delete'),
-    path('', views.product_list, name='product_list'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
+                  path('parse/', views.parse, name='parse'),
+                  path('filter123/', views.filter123, name='filter123'),
+                  path('test/', views.test, name='test'),
+                  path('catalog/', views.catalog, name='catalog'),
+                  path('plan/', views.plan, name='plan'),
+                  path('product_add/', views.product_add, name='product_add'),
+                  path('product_change/', views.product_change, name='product_change'),
+                  path('get_delivery/<int:pk>', views.get_delivery, name='get_delivery'),
+                  path('cart/', views.cart, name='cart'),
+                  path('cart/delete/<int:pk>', views.cart_delete, name='cart_delete'),
+                  path('', views.product_list, name='product_list'),
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
